6835fddba7654703c4b29c37f275a3b3e416731f
Jeffe <j.garner200@gmail.com>
2023-03-26 00:32:11 -0400

    react + vite bare bones

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      <h1>Hello! Welcome to the boilerplate!</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+const container = document.getElementById('root')
+
+if(container){
+  const reactRoot = ReactDOM.createRoot(container)
+  
+  reactRoot.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+} else {
+  console.error('React root is missing from index.html')
+}
