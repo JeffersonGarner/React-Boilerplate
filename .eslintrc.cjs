1e0fcd6c1a17eac06fcb64faeefb26bd8273b581
Jeffe <j.garner200@gmail.com>
2023-03-26 03:16:47 -0400

    sorting object properties and imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,19 +3,26 @@ const eslintConfig = {
         browser: true,
         es2021: true,
     },
-    settings: {
-        react: {
-            pragma: 'React',
-            fragment: 'Fragment',
-            version: 'detect',
-        },
-    },
     extends: [
         'plugin:react/recommended',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'react-app',
     ],
+    overrides: [
+        {
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            files: [
+                '*.ts',
+                '*.tsx' 
+            ],
+            parserOptions: { project: [ './tsconfig.json' ], },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
@@ -23,118 +30,135 @@ const eslintConfig = {
     plugins: [
         'react',
         'react-hooks',
-        '@typescript-eslint' 
+        '@typescript-eslint',
+        'sort-keys-fix',
+        'sort-imports-es6-autofix'
     ],
     rules: {
-        'no-unused-vars': [
-            'error',
-            {
-                vars: 'all',
-                args: 'after-used',
-                ignoreRestSiblings: false,
-            },
-        ],
-        'space-before-function-paren': [
-            'error',
-            {
-                anonymous: 'always',
-                named: 'never',
-                asyncArrow: 'never',
-            },
-        ],
-        'array-element-newline': [
-            'error',
-            {
-                'multiline': true,
-                'minItems': 2 
-            } 
-        ],
+        '@typescript-eslint/indent': 'error',
         'array-bracket-newline': [
             'error',
             {
-                'multiline': true,
-                'minItems': 2 
+                'minItems': 2,
+                'multiline': true 
             } 
-        ],
-        'object-property-newline': [
-            'error',
-            { 'allowAllPropertiesOnSameLine': false } 
-        ],
-        'object-curly-newline': [
-            'error',
-            {
-                'ObjectExpression': {
-                    'multiline': true,
-                    'minProperties': 3 
-                },
-                'ObjectPattern': {
-                    'multiline': true,
-                    'minProperties': 3 
-                },
-                'ImportDeclaration': {
-                    'multiline': true,
-                    'minProperties': 3 
-                },
-                'ExportDeclaration': {
-                    'multiline': true,
-                    'minProperties': 3 
-                }
-            }
-        ],
-        semi: [
-            2,
-            'never' 
-        ],
-        'indent': 'off',
-        'quotes': [
-            'error',
-            'single' 
-        ],
-        '@typescript-eslint/indent': 'error',
-        'react/jsx-uses-react': 'off',
-        'react/react-in-jsx-scope': 'off',
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'error',
-        'react/prop-types': 2,
-        'object-curly-spacing': [
-            'error',
-            'always' 
         ],
         'array-bracket-spacing': [
             'error',
             'always' 
         ],
-        'space-in-parens': [
+        'array-element-newline': [
             'error',
-            'never' 
+            {
+                'minItems': 2,
+                'multiline': true 
+            } 
+        ],
+        'indent': 'off',
+        'jsx-quotes': [
+            2,
+            'prefer-single'
         ],
         'keyword-spacing': [
             'error',
             {
                 'overrides': {
-                    'if': { 'after': false },
+                    'as': { 'after': false },
                     'for': { 'after': false },
-                    'while': { 'after': false },
-                    'static': { 'after': false },
-                    'as': { 'after': false }
+                    'if': { 'after': false },
+                    'static': { 'after': true },
+                    'while': { 'after': false }
                 } 
             }
-        ]
+        ],
+        'no-unused-vars': [
+            'error',
+            {
+                args: 'after-used',
+                ignoreRestSiblings: false,
+                vars: 'all',
+            },
+        ],
+        'object-curly-newline': [
+            'error',
+            {
+                'ExportDeclaration': {
+                    'minProperties': 3,
+                    'multiline': true 
+                },
+                'ImportDeclaration': {
+                    'minProperties': 3,
+                    'multiline': true 
+                },
+                'ObjectExpression': {
+                    'minProperties': 3,
+                    'multiline': true 
+                },
+                'ObjectPattern': {
+                    'minProperties': 3,
+                    'multiline': true 
+                }
+            }
+        ],
+        'object-curly-spacing': [
+            'error',
+            'always' 
+        ],
+        'object-property-newline': [
+            'error',
+            { 'allowAllPropertiesOnSameLine': false } 
+        ],
+        'react-hooks/exhaustive-deps': 'error',
+        'react-hooks/rules-of-hooks': 'error',
+        'react/jsx-uses-react': 'off',
+        'react/prop-types': 2,
+        'react/react-in-jsx-scope': 'off',
+        semi: [
+            2,
+            'never' 
+        ],
+        'sort-imports-es6-autofix/sort-imports-es6': [
+            2,
+            {
+                'ignoreCase': false,
+                'ignoreMemberSort': false,
+                'memberSyntaxSortOrder': [
+                    'all',
+                    'multiple',
+                    'single',
+                    'none',
+                ]
+            }
+        ],
+        'sort-keys': [
+            'error',
+            'asc',
+            {
+                'caseSensitive': true,
+                'natural': false,
+            }
+        ],
+        'sort-keys-fix/sort-keys-fix': 'error',
+        'space-before-function-paren': [
+            'error',
+            {
+                anonymous: 'always',
+                asyncArrow: 'never',
+                named: 'never',
+            },
+        ],
+        'space-in-parens': [
+            'error',
+            'never' 
+        ],
     },
-    parser: '@typescript-eslint/parser',
-    overrides: [
-        {
-            files: [
-                '*.ts',
-                '*.tsx' 
-            ],
-            extends: [
-                'plugin:@typescript-eslint/recommended',
-                'plugin:@typescript-eslint/recommended-requiring-type-checking',
-            ],
-            parserOptions: { project: [ './tsconfig.json' ], },
+    settings: {
+        react: {
+            fragment: 'Fragment',
+            pragma: 'React',
+            version: 'detect',
         },
-    ],
+    },
 }
 
 module.exports = eslintConfig
