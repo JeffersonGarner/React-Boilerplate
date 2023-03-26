129eac371302adae11cb5b485d0e8da0e2348af8
Jeffe <j.garner200@gmail.com>
2023-03-26 13:01:56 -0400

    cleanup and readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,134 @@
+const rules = {
+    '@typescript-eslint/indent': 'error',
+    'array-bracket-newline': [
+        'error',
+        {
+            'minItems': 2,
+            'multiline': true 
+        } 
+    ],
+    'array-bracket-spacing': [
+        'error',
+        'always' 
+    ],
+    'array-element-newline': [
+        'error',
+        {
+            'minItems': 2,
+            'multiline': true 
+        } 
+    ],
+    'indent': 'off',
+    'keyword-spacing': [
+        'error',
+        {
+            'overrides': {
+                'as': { 'after': false },
+                'for': { 'after': false },
+                'if': { 'after': false },
+                'static': { 'after': true },
+                'while': { 'after': false }
+            } 
+        }
+    ],
+    'no-multi-spaces': 2,
+    'no-multiple-empty-lines': [
+        'error',
+        { 'max': 1 }
+    ],
+    'no-unused-vars': [
+        'error',
+        {
+            args: 'after-used',
+            ignoreRestSiblings: false,
+            vars: 'all',
+        },
+    ],
+    'object-curly-newline': [
+        'error',
+        {
+            'ExportDeclaration': {
+                'minProperties': 3,
+                'multiline': true 
+            },
+            'ImportDeclaration': {
+                'minProperties': 3,
+                'multiline': true 
+            },
+            'ObjectExpression': {
+                'minProperties': 3,
+                'multiline': true 
+            },
+            'ObjectPattern': {
+                'minProperties': 3,
+                'multiline': true 
+            }
+        }
+    ],
+    'object-curly-spacing': [
+        'error',
+        'always' 
+    ],
+    'object-property-newline': [
+        'error',
+        { 'allowAllPropertiesOnSameLine': false } 
+    ],
+    'quotes': [
+        2,
+        'single'
+    ],
+    'react-hooks/exhaustive-deps': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 2,
+    'react/react-in-jsx-scope': 'off',
+    semi: [
+        2,
+        'never' 
+    ],
+    'sonarjs/cognitive-complexity': 'error',
+    'sonarjs/no-all-duplicated-branches': 'error',
+    'sonarjs/no-gratuitous-expressions': 'error',
+    'sonarjs/no-identical-functions': [
+        'error',
+        3
+    ],
+    'sort-imports-es6-autofix/sort-imports-es6': [
+        2,
+        {
+            'ignoreCase': false,
+            'ignoreMemberSort': false,
+            'memberSyntaxSortOrder': [
+                'all',
+                'multiple',
+                'single',
+                'none',
+            ]
+        }
+    ],
+    'sort-keys': [
+        'error',
+        'asc',
+        {
+            'caseSensitive': true,
+            'natural': false,
+        }
+    ],
+    'sort-keys-fix/sort-keys-fix': 'error',
+    'space-before-function-paren': [
+        'error',
+        {
+            anonymous: 'always',
+            asyncArrow: 'never',
+            named: 'never',
+        },
+    ],
+    'space-in-parens': [
+        'error',
+        'never' 
+    ],
+}
+
 const eslintConfig = {
     env: {
         browser: true,
@@ -36,136 +167,7 @@ const eslintConfig = {
         'sort-imports-es6-autofix',
         'sonarjs'
     ],
-    rules: {
-        '@typescript-eslint/indent': 'error',
-        'array-bracket-newline': [
-            'error',
-            {
-                'minItems': 2,
-                'multiline': true 
-            } 
-        ],
-        'array-bracket-spacing': [
-            'error',
-            'always' 
-        ],
-        'array-element-newline': [
-            'error',
-            {
-                'minItems': 2,
-                'multiline': true 
-            } 
-        ],
-        'indent': 'off',
-        'keyword-spacing': [
-            'error',
-            {
-                'overrides': {
-                    'as': { 'after': false },
-                    'for': { 'after': false },
-                    'if': { 'after': false },
-                    'static': { 'after': true },
-                    'while': { 'after': false }
-                } 
-            }
-        ],
-        'no-multi-spaces': 2,
-        'no-multiple-empty-lines': [
-            'error',
-            { 'max': 1 }
-        ],
-        'no-unused-vars': [
-            'error',
-            {
-                args: 'after-used',
-                ignoreRestSiblings: false,
-                vars: 'all',
-            },
-        ],
-        'object-curly-newline': [
-            'error',
-            {
-                'ExportDeclaration': {
-                    'minProperties': 3,
-                    'multiline': true 
-                },
-                'ImportDeclaration': {
-                    'minProperties': 3,
-                    'multiline': true 
-                },
-                'ObjectExpression': {
-                    'minProperties': 3,
-                    'multiline': true 
-                },
-                'ObjectPattern': {
-                    'minProperties': 3,
-                    'multiline': true 
-                }
-            }
-        ],
-        'object-curly-spacing': [
-            'error',
-            'always' 
-        ],
-        'object-property-newline': [
-            'error',
-            { 'allowAllPropertiesOnSameLine': false } 
-        ],
-        'quotes': [
-            2,
-            'single'
-        ],
-        'react-hooks/exhaustive-deps': 'error',
-        'react-hooks/rules-of-hooks': 'error',
-        'react/jsx-uses-react': 'off',
-        'react/prop-types': 2,
-        'react/react-in-jsx-scope': 'off',
-        semi: [
-            2,
-            'never' 
-        ],
-        'sonarjs/cognitive-complexity': 'error',
-        'sonarjs/no-all-duplicated-branches': 'error',
-        'sonarjs/no-gratuitous-expressions': 'error',
-        'sonarjs/no-identical-functions': [
-            'error',
-            3
-        ],
-        'sort-imports-es6-autofix/sort-imports-es6': [
-            2,
-            {
-                'ignoreCase': false,
-                'ignoreMemberSort': false,
-                'memberSyntaxSortOrder': [
-                    'all',
-                    'multiple',
-                    'single',
-                    'none',
-                ]
-            }
-        ],
-        'sort-keys': [
-            'error',
-            'asc',
-            {
-                'caseSensitive': true,
-                'natural': false,
-            }
-        ],
-        'sort-keys-fix/sort-keys-fix': 'error',
-        'space-before-function-paren': [
-            'error',
-            {
-                anonymous: 'always',
-                asyncArrow: 'never',
-                named: 'never',
-            },
-        ],
-        'space-in-parens': [
-            'error',
-            'never' 
-        ],
-    },
+    rules,
     settings: {
         react: {
             fragment: 'Fragment',
---
+++ README.md
@@ -0,0 +1,8 @@
+#What
+
+A React boilerplate for quicker SPA prototyping and proofs of concept
+
+#Notes
+
+- Styled Components: Not fantastic for SSR
+- Somewhat opinionated ESLint. Unlike prettier, there's more control. 
